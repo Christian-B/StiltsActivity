f272063c0629d649a1b701dfbff93d6542917c82
Christian-B <brenninc@cs.man.ac.uk>
2014-03-05 12:28:27 +0000

    Added a test which needs a Activity change (commeneted out)

--- stilts-activity/src/test/java/net/sf/taverna/t2/activities/stilts/MatchActivityTest.java
+++ stilts-activity/src/test/java/net/sf/taverna/t2/activities/stilts/MatchActivityTest.java
@@ -47,7 +47,7 @@ public class MatchActivityTest {
     
     @Test
     public void executeAsynch() throws Exception {
-        System.out.println("Running TCat");
+        System.out.println("executeAsynch");
         activity.configure(configBean);
 
         Map<String, Object> inputs = new HashMap<String, Object>();
@@ -79,6 +79,41 @@ public class MatchActivityTest {
         assertTrue("Wrong output : Brown line missing. ", result.contains("2,Brown,test@example.com,Boss,2,Jack,456"));
     }
 
+/*    @Test
+    public void doubleMatch() throws Exception {
+        System.out.println("doubleMatch");
+        activity.configure(configBean);
+
+        Map<String, Object> inputs = new HashMap<String, Object>();
+        inputs.put(MultipleFormatsActivity.INPUT_PARAMETER_NAME+1, 
+                "code,first, Last,Email,Job" + System.lineSeparator() + 
+                "1,Peter,Smith,test@example.com,Programmer" + System.lineSeparator() + 
+                "2,Jack,Brown,test@example.com,Boss");
+        inputs.put(MultipleFormatsActivity.INPUT_PARAMETER_NAME+2, 
+                "id,name,number" + System.lineSeparator() + 
+                "1,Peter,1433" + System.lineSeparator() + 
+                "2,Peter,1433" + System.lineSeparator() + 
+                "2,Jack,456");
+        inputs.put(MatchActivity.MACTH_COLUMNS_TABLE1_PARAMETER_NAME,"code first"); 
+        inputs.put(MatchActivity.MACTH_COLUMNS_TABLE2_PARAMETER_NAME,"id name"); 
+
+        Map<String, Class<?>> expectedOutputTypes = new HashMap<String, Class<?>>();
+        expectedOutputTypes.put(AbstractStilsActivity.RESULT_PARAMETER_NAME, String.class);
+        //expectedOutputTypes.put("moreOutputs", String.class);
+
+        Map<String, Object> outputs = ActivityInvoker.invokeAsyncActivity(
+                activity, inputs, expectedOutputTypes);
+                
+        System.out.println("Run done");
+        assertEquals("Unexpected outputs", 1, outputs.size());
+        //assertEquals("simple", outputs.get("simpleOutput"));
+        String result = outputs.get(AbstractStilsActivity.RESULT_PARAMETER_NAME).toString();
+        System.out.println(result);
+        assertTrue("Wrong output : Header line missing. ", result.contains("code,Last,Email,Job,id,name,number"));
+        assertTrue("Wrong output : Smith line missing. ", result.contains("1,Smith,test@example.com,Programmer,1,Peter,1433"));
+        assertTrue("Wrong output : Brown line missing. ", result.contains("2,Brown,test@example.com,Boss,2,Jack,456"));
+    }
+*/
     @Test
     public void reConfiguredActivity() throws Exception {
         assertEquals("Unexpected inputs", 0, activity.getInputPorts().size());
